8d9b5f9462942bdbb6381773bd4dea918af4eb40
Marc Schnathmeier <ms730638@fh-muenster.de>
2026-01-25 12:16:24 +0100

    Day 8 part 1 naive solution

---
+++ solutions/08.cpp
@@ -0,0 +1,143 @@
+// Advent of Code Day 8
+// https://adventofcode.com/2025/day/8
+
+#include <iomanip>
+#include <iostream>
+#include <cstdint>
+#include <expected>
+#include <filesystem>
+#include <fstream>
+#include <print>
+#include <string>
+#include <string_view>
+#include <ranges>
+#include <array>
+#include <vector>
+#include <cstdlib>
+#include <charconv>
+#include <numeric>
+#include <algorithm>
+#include <tuple>
+
+struct JBox {
+    int64_t x;
+    int64_t y;
+    int64_t z;
+    int id;
+};
+
+struct Edge {
+    size_t u;
+    size_t v;
+    int64_t dist;
+
+    auto operator<=>(const Edge&) const = default;
+};
+
+// disjoint set union for union find to track circuits 
+struct DSU {
+    std::vector<int> parent;
+    std::vector<int> size;
+
+    DSU(int n) {
+        parent.resize(n);
+        std::iota(parent.begin(), parent.end(), 0);
+        size.assign(n, 1);
+    }
+
+    int find(int i) {
+        if (parent[i] == i)
+            return i;
+        return parent[i] = find(parent[i]);
+    }
+
+    void unite(int i, int j) {
+        i = find(i);
+        j = find(j);
+        if (i != j) {
+            if (size[i] < size[j])
+                std::swap(i, j);
+            parent[j] = i;
+            size[i] += size[j];
+        }
+    }
+};
+
+int64_t dist(const JBox& p, const JBox& q) {
+    // we just need to compare the distances, so doing the expensize sqrt is actually not necessary,
+    // as its monotonic
+    return (p.x - q.x)*(p.x - q.x) + (p.y - q.y)*(p.y - q.y) + (p.z - q.z)*(p.z - q.z);
+}
+
+int main(int argc, char** argv) {
+    const std::filesystem::path input_path =
+        (argc > 1)  ? std::filesystem::path{argv[1]}
+                    : std::filesystem::path{"../inputs/input_08.txt"};
+
+    std::ifstream input_file{input_path};
+    if (!input_file.is_open()) {
+        std::println(stderr, "Failed to open input file: {}", input_path.string());
+        return EXIT_FAILURE;
+    }
+    
+    std::string line;
+    std::vector<JBox> junctions{};
+    junctions.reserve(2000);
+    int current_id = 0;
+    // not the cleanest looking but probably the fastest, as the input format is known
+    while (getline(input_file, line)) {
+        if (line.empty()) break;
+        JBox j;
+        j.id = current_id++;
+        const char* ptr = line.data();
+        const char* end = ptr + line.size();
+
+        auto [ptr1, ec1] = std::from_chars(ptr, end, j.x);
+        auto [ptr2, ec2] = std::from_chars(ptr1 + 1, end, j.y);
+        std::from_chars(ptr2 + 1, end, j.z);
+
+        junctions.push_back(j);
+    }
+
+    // generate all edges
+    std::vector<Edge> edges;
+    // pre-calculate size: N * (N-1) / 2
+    size_t n = junctions.size();
+    edges.reserve(n * (n - 1) / 2);
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = i + 1; j < n; j++) {
+            edges.push_back({i, j, dist(junctions[i], junctions[j])});
+        }
+    }
+
+    // sort by distance
+    std::sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
+        return a.dist < b.dist;
+    });
+
+    DSU dsu(n);
+    // connect 1000 closest
+    size_t limit = std::min(edges.size(), static_cast<size_t>(1000));
+
+    for (size_t i = 0; i < limit; ++i) {
+        dsu.unite(edges[i].u, edges[i].v);
+    }
+
+    std::vector<int64_t> circuit_sizes;
+    for (int i = 0; i < n; ++i) {
+        // Since we tracked size in the root, we only take sizes from root nodes
+        if (dsu.parent[i] == i) {
+            circuit_sizes.push_back(dsu.size[i]);
+        }
+    }
+
+    // Sort descending to find large clusters
+    std::sort(circuit_sizes.rbegin(), circuit_sizes.rend());
+
+    int64_t result = circuit_sizes[0] * circuit_sizes[1] * circuit_sizes[2];
+    std::println("Top 3 cluster sizes: {}, {}, {}", circuit_sizes[0], circuit_sizes[1], circuit_sizes[2]);
+    std::println("Part 1 Result: {}", result);
+
+    return EXIT_SUCCESS;
+}
